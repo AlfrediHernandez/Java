8e10e417cf22700e4b4a87a9587f1f83a7df83da
AlfrediHdz <alfredi1963@hotmail.com>
2020-08-29 10:42:37 -0500

    se crea el programita

---
+++ Aleatorios.java
@@ -0,0 +1,8 @@
+public class Aleatorios{
+	public static void main(String [] ar){
+		int alea;
+		alea = (int)(Math.random()*1000);
+		System.out.print(alea);
+		System.out.print("=  AlfrediHdz 29-ago-2020  =");
+	}
+}
