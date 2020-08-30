f80bf12d2f5351c937b5a625ee5720e93efef34e
AlfrediHdz <alfredi1963@hotmail.com>
2020-08-30 10:23:03 -0500

    Se agrego Restas

---
+++ Restas.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Restas{
+	public static void main(String [] ar){
+		Scanner sc = new Scanner(System.in);
+		int a,b,r;
+		System.out.println("*******************************");
+		System.out.println("*           RESTAS            *");
+		System.out.println("*******************************");
+		System.out.print("Ingrese a:	");
+			a = sc.nextInt();
+		System.out.print("Ingrese b:	");
+			b = sc.nextInt();
+			r = a - b;
+			System.out.println("       --------");
+			System.out.println("Resultado:	"+r);
+			System.out.println("                            ");
+			System.out.println("****************************");
+			System.out.println("*  AlfrediHdz 30-ago-2020  *");
+			System.out.println("****************************");
+	}
+}
