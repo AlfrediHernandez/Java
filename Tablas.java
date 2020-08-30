d7d42d9962b738e34e4c79cda726c5b50ae567e8
AlfrediHdz <alfredi1963@hotmail.com>
2020-08-30 08:56:47 -0500

    Se termina el programita

--- Tablas.java
+++ Tablas.java
@@ -1,1 +1,18 @@
 import java.util.Scanner;
+public class Tablas{
+	public static void main(String [] ar){
+		Scanner sc = new Scanner(System.in);
+		int tab,r;
+
+		system.out.println("Ingrese la tabla que desea ver:");
+			tab = sc.nextInt();
+			for(int a=1; a<=9; a++){
+				r = tab * a;
+				System.out.println(tab + " X " + a + " = "+r);
+		}
+		System.out.println("                                  ");
+		System.out.println("**********************************");
+		System.out.println("*  AlfrediHernandez 30-ago-2020  *");
+		System.out.println("**********************************");
+	}
+}
