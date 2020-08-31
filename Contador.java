f76cb8ff6f026226d1958b9502f8ed00fc944c16
AlfrediHdz <alfredi1963@hotmail.com>
2020-08-31 09:54:26 -0500

    Se agrega class a Piramide y se Crea Contado programita

---
+++ Contador.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Contador{
+	public static void main(String [] ar){
+		Scanner sc = new Scanner(System.in);
+		int cont;
+		System.out.println("======================================");
+		System.out.println("=              CONTADORES            =");
+		System.out.println("======================================");
+		System.out.println("Ingrese un nuumero para el Contador:");
+			cont = sc.nextInt();
+			for(int a=1; a<=cont; a++){
+				System.out.println("Contando...  "+a);
+		}
+		System.out.println("                                       ");
+		System.out.println("=======================================");
+		System.out.println("=     AlfrediHernandez 31-ago-2020    =");
+		System.out.println("=======================================");
+	}
+}
