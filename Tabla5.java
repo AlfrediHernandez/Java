ee5e007e1ab8f71a9ce531e98269a33db5db89ee
AlfrediHdz <alfredi1963@hotmail.com>
2020-09-01 13:49:15 -0500

    Se agrega Tabla del 5 al Programita

---
+++ Tabla5.java
@@ -0,0 +1,16 @@
+public class Tabla5{
+	public static void main(String [] ar){
+		int tab=5,r;
+		System.out.println("=====================================");
+		System.out.println("=            TABLA DEL 5            =");
+		System.out.println("=====================================");
+			for(int a=1; a<=9; a++){
+				r = tab * a;
+				System.out.println(tab + " X " + a + " = "+r);
+			}
+			System.out.println("                                 ");
+			System.out.println("=================================");
+			System.out.println("=  AlfrediHernandez 01-sep-2020 =");
+			System.out.println("=================================");
+	}
+}
